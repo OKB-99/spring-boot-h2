7ba64cd4699785fa580abb8b3e05237ac862afef
Takumi Ishida <takumi.ishida@eng.it>
2021-04-26 02:36:42 +0200

    Fix password in data.sql, schema.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,4 +2,4 @@ INSERT INTO persons (id, first_name, last_name, email) VALUES (1, 'Hahn', 'Booke
 INSERT INTO persons (id, first_name, last_name, email) VALUES (2, 'Kimberlee', 'Stiedemann', 'kimberlee.stiedemann@example.com');
 INSERT INTO persons (id, first_name, last_name, email) VALUES (3, 'Rubi', 'Muller', 'rubi.muller@example.com');
 
-INSERT INTO users (id, username, password, user_role, person_id) VALUES (1, 'admin', '12345', 'ADMIN', 1);
+INSERT INTO users (id, username, password, user_role, person_id) VALUES (1, 'admin', '$2y$12$ZrBfWY6v6xN6uo1BUdWmfeAfMBoCdlduskOffcXwRTxRD8NfkOENu', 'ADMIN', 1);
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS persons (
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT ,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
     user_role VARCHAR(10),
     person_id INT NOT NULL,
     FOREIGN KEY(person_id) REFERENCES persons(id)
