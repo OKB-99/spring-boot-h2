a77a9765ed6f1fe925f135f441852e28aeee2981
Takumi Ishida <takumi.ishida@eng.it>
2021-05-07 01:22:45 +0200

    Add UserService::updatePassword().

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UpdatePasswordRequest;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +18,18 @@ public class UserService {
   }
 
   public User save(User user) {
+    return userRepository.save(user);
+  }
+
+  public User updatePassword(UpdatePasswordRequest updatePasswordRequest) {
+    User user = userRepository.findById(updatePasswordRequest.getId()).get();
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    if (!user.getPassword().equals(passwordEncoder.encode(updatePasswordRequest.getOldPassword())))
+      throw new RuntimeException("Password is NOT correct!");
+    else if (updatePasswordRequest.getNewPasswordOne().equals(updatePasswordRequest.getNewPasswordTwo()))
+      throw new RuntimeException("New password is Mismatched!");
+
+    user.setPassword(passwordEncoder.encode(updatePasswordRequest.getNewPasswordOne()));
     return userRepository.save(user);
   }
 }
