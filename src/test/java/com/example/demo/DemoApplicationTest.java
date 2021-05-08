a040b5ed1d1438cc4a7982348955bccbc322989e
Takumi Ishida <takumi.ishida@eng.it>
2021-05-09 01:54:10 +0200

    Add DemoApplicationTest

--- src/test/java/com/example/demo/DemoApplicationTest.java
+++ src/test/java/com/example/demo/DemoApplicationTest.java
@@ -1,4 +1,40 @@
 package com.example.demo;
 
-public class DemoApplicationTest {
+import com.example.demo.model.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+public class DemoApplicationTest extends AbstractJUnit4SpringContextTests {
+
+  @Test
+  public void outputBean() {
+    System.out.println("## Test Case 1");
+    for (String bean: this.applicationContext.getBeanDefinitionNames()) {
+      System.out.println("Bean: " + bean);
+    }
+  }
+
+  @Test
+  public void restGetRequest() {
+    RestTemplate template = new RestTemplate();
+    Person[] response = template.getForObject("http://localhost:8080/rest/persons", Person[].class);
+    for (Person person: response) {
+      System.out.println(person.toString());
+    }
+  }
+
+  @Test
+  public void restPostRequest() {
+    RestTemplate template = new RestTemplate();
+    Person person = new Person();
+    person.setLastName("Dyson");
+    person.setFirstName("Mike");
+    person.setEmail("mike.dyson@exapmple.com");
+
+    Person returnedPerson = template.postForObject("http://localhost:8080/rest/persons/insert", person, Person.class);
+    System.out.println(returnedPerson.toString());
+  }
 }
